ae0b3683f7dde1834e8dad9f29234281c7f4beac
Atte Lautanala <atte.lautanala@gmail.com>
2018-07-29 16:39:26 +0300

    Define structure for version 3 sensor data

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+pub enum SensorData {
+    Version3 {
+        humidity: u8,
+        temperature: i8,
+        temperature_fraction: i8,
+        pressure: u16,
+        acceleration: AccelerationVector,
+        battery_voltage: u16
+    },
+}
+
+pub struct AccelerationVector(i16, i16, i16);
+
 #[cfg(test)]
 mod tests {
     #[test]
