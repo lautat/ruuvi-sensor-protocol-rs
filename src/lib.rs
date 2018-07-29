dca77f8a43579e577efc57cf944345c4bef63616
Atte Lautanala <atte.lautanala@gmail.com>
2018-07-29 16:59:08 +0300

    Define interface for parsing sensor data

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::{self, Display, Formatter}};
+
 pub enum SensorData {
     Version3 {
         humidity: u8,
@@ -5,11 +7,35 @@ pub enum SensorData {
         temperature_fraction: i8,
         pressure: u16,
         acceleration: AccelerationVector,
-        battery_voltage: u16
+        battery_voltage: u16,
     },
 }
 
+impl SensorData {
+    pub fn parse_from_manufacturer_specific_data(
+        _id: u16,
+        _value: &[u8],
+    ) -> Result<Self, ParseError> {
+        unimplemented!();
+    }
+}
+
 pub struct AccelerationVector(i16, i16, i16);
+
+#[derive(Debug)]
+pub enum ParseError {
+    UnknownManufacturerId(u16),
+    UnknownDataFormat(u8, usize),
+}
+
+impl Display for ParseError {
+    fn fmt(&self, _f: &mut Formatter) -> Result<(), fmt::Error> {
+        unimplemented!();
+    }
+}
+
+impl Error for ParseError {
+}
 
 #[cfg(test)]
 mod tests {
