75105885fb77418d2d7b602178954a8a74e26114
Atte Lautanala <atte.lautanala@gmail.com>
2018-07-29 17:11:23 +0300

    Define error messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, fmt::{self, Display, Formatter}};
+use std::{
+    error::Error, fmt::{self, Display, Formatter},
+};
+
+use self::ParseError::*;
 
 pub enum SensorData {
     Version3 {
@@ -25,17 +29,41 @@ pub struct AccelerationVector(i16, i16, i16);
 #[derive(Debug)]
 pub enum ParseError {
     UnknownManufacturerId(u16),
-    UnknownDataFormat(u8, usize),
+    UnknownDataFormatVersion(u8),
+    InvalidDataLength {
+        version: u8,
+        length: usize,
+        expected: usize,
+    },
 }
 
 impl Display for ParseError {
-    fn fmt(&self, _f: &mut Formatter) -> Result<(), fmt::Error> {
-        unimplemented!();
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), fmt::Error> {
+        match self {
+            UnknownManufacturerId(id) => write!(
+                formatter,
+                "Unknown manufacturer id {:#04X}, only 0x0499 is supported",
+                id
+            ),
+            UnknownDataFormatVersion(data_format) => write!(
+                formatter,
+                "Unknown data format version {}, only version 3 is supported",
+                data_format
+            ),
+            InvalidDataLength {
+                version,
+                length,
+                expected,
+            } => write!(
+                formatter,
+                "Invalid data length of {} for format version {}, expected length of {}",
+                length, version, expected
+            ),
+        }
     }
 }
 
-impl Error for ParseError {
-}
+impl Error for ParseError {}
 
 #[cfg(test)]
 mod tests {
