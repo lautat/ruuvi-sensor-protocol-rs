81aa32bd44c1f5117af97cdf995204873ecd8848
Atte Lautanala <atte.lautanala@gmail.com>
2018-07-31 21:33:17 +0300

    Parse all version 3 sensor data

--- src/sensordata/v3.rs
+++ src/sensordata/v3.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct SensorDataV3 {
     humidity: u8,
     temperature: u16,
@@ -7,26 +7,35 @@ pub struct SensorDataV3 {
     battery_potential: u16,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct AccelerationVectorV3(i16, i16, i16);
 
 impl SensorDataV3 {
     pub fn from_manufacturer_specific_data(value: &[u8]) -> Result<Self, InvalidValueLength> {
         if value.len() == 14 {
-            let humidity = value[1];
-            let temperature = ((value[2] as u16) << 8) | value[3] as u16;
-
             Ok(Self {
-                humidity,
-                temperature,
-                pressure: 0,
-                acceleration: AccelerationVectorV3(0, 0, 0),
-                battery_potential: 0,
+                humidity: value[1],
+                temperature: u16_from_two_bytes(value[2], value[3]),
+                pressure: u16_from_two_bytes(value[4], value[5]),
+                acceleration: AccelerationVectorV3(
+                    i16_from_two_bytes(value[6], value[7]),
+                    i16_from_two_bytes(value[8], value[9]),
+                    i16_from_two_bytes(value[10], value[11]),
+                ),
+                battery_potential: u16_from_two_bytes(value[12], value[13]),
             })
         } else {
             Err(InvalidValueLength)
         }
     }
+}
+
+fn u16_from_two_bytes(b1: u8, b2: u8) -> u16 {
+    ((b1 as u16) << 8) | b2 as u16
+}
+
+fn i16_from_two_bytes(b1: u8, b2: u8) -> i16 {
+    u16_from_two_bytes(b1, b2) as i16
 }
 
 #[derive(Debug, PartialEq)]
@@ -40,23 +49,24 @@ mod tests {
     fn parse_version_3_data_with_invalid_length() {
         let value = vec![3, 103, 22, 50, 60, 70];
         let result = SensorDataV3::from_manufacturer_specific_data(&value);
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), InvalidValueLength);
+        assert_eq!(result, Err(InvalidValueLength));
     }
 
     #[test]
-    fn parse_version_3_humidity() {
-        let value = vec![3, 0x17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    fn parse_valid_version_3_data() {
+        let value = vec![
+            3, 0x17, 0x01, 0x45, 0x35, 0x58, 0x03, 0xE8, 0x04, 0xE7, 0x05, 0xE6, 0x08, 0x86,
+        ];
         let result = SensorDataV3::from_manufacturer_specific_data(&value);
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().humidity, 0x17);
-    }
-
-    #[test]
-    fn parse_version_3_temperature() {
-        let value = vec![3, 0, 0x01, 0x45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        let result = SensorDataV3::from_manufacturer_specific_data(&value);
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().temperature, 0x0145);
+        assert_eq!(
+            result,
+            Ok(SensorDataV3 {
+                humidity: 0x17,
+                temperature: 0x0145,
+                pressure: 0x3558,
+                acceleration: AccelerationVectorV3(1000, 1255, 1510),
+                battery_potential: 0x0886
+            })
+        );
     }
 }
