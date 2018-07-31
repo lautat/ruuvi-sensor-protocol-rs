693d3585855a59f4bbd7c17b4d9eccc301f2c815
Atte Lautanala <atte.lautanala@gmail.com>
2018-07-31 21:38:53 +0300

    Define generic sensor data struct
    Version 5 compatibility is taken into account in the design of this
    structure by keeping all fields optional. Version 5 has specific values
    for each field which marks the reading as not available.

--- src/sensordata/mod.rs
+++ src/sensordata/mod.rs
@@ -1,1 +1,13 @@
 mod v3;
+
+#[derive(Debug)]
+pub struct SensorData {
+    humidity: Option<u32>,
+    temperature: Option<u32>,
+    pressure: Option<u32>,
+    acceleration: Option<AccelerationVector>,
+    battery_potential: Option<u16>
+}
+
+#[derive(Debug)]
+pub struct AccelerationVector(i16, i16, i16);
