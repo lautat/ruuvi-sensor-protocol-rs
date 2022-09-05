3eaf39024cadaf3b2145c5ae6a7fffc3ced41acd
Atte Lautanala <atte.lautanala@gmail.com>
2022-09-05 23:05:59 +0300

    Add data parsing tests

--- src/gateway/mqtt.rs
+++ src/gateway/mqtt.rs
@@ -12,4 +12,35 @@ mod tests {
     fn mqtt_data_has_default_traits() {
         crate::testing::type_has_default_traits::<MqttData>();
     }
+
+    #[test]
+    fn parse_mqtt_data_counter() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"0201061BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
+
+    #[test]
+    fn parse_mqtt_data_timestamps() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"ts\": \"1653668027\",
+            \"gwts\": \"1653668027\",
+            \"data\": \"0201061BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
 }
