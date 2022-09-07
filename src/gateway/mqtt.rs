656446e0a7ee6aca88cd7ac82f1744aca226b481
Atte Lautanala <atte.lautanala@gmail.com>
2022-09-07 20:17:23 +0300

    Add more tests with different data arrays

--- src/gateway/mqtt.rs
+++ src/gateway/mqtt.rs
@@ -43,4 +43,80 @@ mod tests {
         ";
         let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
     }
+
+    #[test]
+    fn parse_mqtt_data_switched_packet_order() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"1BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6020106\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
+
+    #[test]
+    fn parse_mqtt_data_no_flags_packet() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"1BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
+
+    #[test]
+    fn parse_mqtt_data_no_manufacturer_data_packet() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"020106\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
+
+    #[test]
+    fn parse_mqtt_data_empty_data() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
+
+    #[test]
+    fn parse_mqtt_data_two_manufacturer_data_packets() {
+        let data = "
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"1BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\
+                        1BFF990405158A5B05C6810004004403DCAB767A45BDE375CF374E23\",
+            \"coords\": \"\"
+        }
+        ";
+        let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
 }
