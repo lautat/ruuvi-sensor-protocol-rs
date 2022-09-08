f7440b4145bb8d749fa660e266b346dd5b26aaaf
Atte Lautanala <atte.lautanala@gmail.com>
2022-09-08 20:51:52 +0300

    Parse sensor values from manufacturer data

--- src/gateway/mqtt.rs
+++ src/gateway/mqtt.rs
@@ -1,26 +1,34 @@
 use serde::de::{Error, Unexpected};
 
-use crate::gateway::data::{IterPackets, Packet};
+use crate::{gateway::data::{IterPackets, Packet}, SensorValues};
 
 #[derive(serde::Deserialize, Debug)]
 struct MqttData {
     #[serde(deserialize_with = "deserialize_data")]
-    data: Vec<u8>,
+    data: SensorValues,
 }
 
 fn deserialize_data<'de, D: serde::Deserializer<'de>>(
     deserializer: D,
-) -> Result<Vec<u8>, D::Error> {
+) -> Result<SensorValues, D::Error> {
     let data: Vec<u8> = hex::serde::deserialize(deserializer)?;
     let mut packets = IterPackets::new(&data);
     let manufacturer_data = packets.try_fold(None, |result, packet| match (result, packet) {
-        (None, Ok(Packet::ManufacturerData(_, data))) => Ok(Some(data)),
+        (None, Ok(Packet::ManufacturerData(id, data))) => Ok(Some((id, data))),
         (_, Err(err)) => Err(err),
         (result, _) => Ok(result),
     });
 
-    if let Ok(Some(data)) = manufacturer_data {
-        Ok(data.to_vec())
+    if let Ok(Some((id, data))) = manufacturer_data {
+        if let Ok(values) = SensorValues::from_manufacturer_specific_data(id, data) {
+            Ok(values)
+        } else {
+            let error = D::Error::invalid_value(
+                Unexpected::Bytes(&data),
+                &"an advertisement containing a valid Ruuvi manufacturer data packet",
+            );
+            Err(error)
+        }
     } else {
         let error = D::Error::invalid_value(
             Unexpected::Bytes(&data),
@@ -41,7 +49,7 @@ mod tests {
 
     #[test]
     fn parse_mqtt_data_counter() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -49,14 +57,14 @@ mod tests {
             \"cnt\": \"338\",
             \"data\": \"0201061BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
     }
 
     #[test]
     fn parse_mqtt_data_timestamps() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -65,14 +73,14 @@ mod tests {
             \"gwts\": \"1653668027\",
             \"data\": \"0201061BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
     }
 
     #[test]
     fn parse_mqtt_data_switched_packet_order() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -80,14 +88,14 @@ mod tests {
             \"cnt\": \"338\",
             \"data\": \"1BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6020106\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
     }
 
     #[test]
     fn parse_mqtt_data_no_flags_packet() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -95,14 +103,14 @@ mod tests {
             \"cnt\": \"338\",
             \"data\": \"1BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
     }
 
     #[test]
     fn parse_mqtt_data_no_manufacturer_data_packet() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -110,7 +118,7 @@ mod tests {
             \"cnt\": \"338\",
             \"data\": \"020106\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let mqtt_data: Result<MqttData, _> = serde_json::from_str(data);
 
@@ -119,7 +127,7 @@ mod tests {
 
     #[test]
     fn parse_mqtt_data_empty_data() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -127,7 +135,7 @@ mod tests {
             \"cnt\": \"338\",
             \"data\": \"\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let mqtt_data: Result<MqttData, _> = serde_json::from_str(data);
 
@@ -136,7 +144,7 @@ mod tests {
 
     #[test]
     fn parse_mqtt_data_two_manufacturer_data_packets() {
-        let data = "
+        let data = "\
         {
             \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
             \"rssi\": -25,
@@ -145,8 +153,42 @@ mod tests {
             \"data\": \"1BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\
                         1BFF990405158A5B05C6810004004403DCAB767A45BDE375CF374E23\",
             \"coords\": \"\"
-        }
+        }\
         ";
         let _mqtt_data: MqttData = serde_json::from_str(data).unwrap();
+    }
+
+    #[test]
+    fn parse_mqtt_data_invalid_manufacturer() {
+        let data = "\
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"0201061BFF9A0405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CBD6\",
+            \"coords\": \"\"
+        }\
+        ";
+        let mqtt_data: Result<MqttData, _> = serde_json::from_str(data);
+
+        assert!(mqtt_data.is_err());
+    }
+
+    #[test]
+    fn parse_mqtt_data_invalid_data() {
+        let data = "\
+        {
+            \"gw_mac\": \"C8:25:2D:8E:9C:2C\",
+            \"rssi\": -25,
+            \"aoa\": [],
+            \"cnt\": \"338\",
+            \"data\": \"0201061BFF990405166455D5C6DE0008FFF403F0AE760F2A8BF41F0C28CB\",
+            \"coords\": \"\"
+        }\
+        ";
+        let mqtt_data: Result<MqttData, _> = serde_json::from_str(data);
+
+        assert!(mqtt_data.is_err());
     }
 }
